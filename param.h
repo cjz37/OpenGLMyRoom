a49b21ea8b3885f5d368041165ba59ce74bc8ebf
Crange <59814753+cjz37@users.noreply.github.com>
2021-12-02 23:57:43 +0800

    Update param.h

--- param.h
+++ param.h
@@ -5,7 +5,7 @@
 GLfloat disH = 0.0;
 GLfloat disV = 0.0;
 GLfloat stride = 1.5;
-GLfloat gloSize = 10.0;
+GLfloat gloSize = 8.5;
 int w = 700;
 int h = 700;
 static GLfloat lightAngle = 90.0;
@@ -17,7 +17,7 @@ int degree = 45;
 int degree2 = 45;
 double c = 3.1415926 / 180.0f;
 bool isDown = false;
-const char* filepaths[] = { "Res/texture/floor1.bmp", "Res/texture/floor2.bmp", "Res/texture/floor3.bmp", "Res/texture/floor4.bmp", "Res/texture/floor5.bmp", "Res/texture/paint1.bmp", "Res/texture/paint2.bmp", "Res/texture/paint3.bmp", "Res/texture/wall1.bmp", "Res/texture/wall2.bmp", "Res/texture/wall3.bmp", "Res/texture/wall4.bmp", "Res/texture/ball.bmp", "Res/texture/carpet.bmp", "Res/texture/wood1.bmp", "Res/texture/wood2.bmp", "Res/texture/quilt.bmp", "Res/texture/door1.bmp", "Res/texture/door2.bmp", "Res/texture/door3.bmp", "Res/texture/doorframe.bmp", "Res/texture/black.bmp" }; //22
+const char* filepaths[] = { "Res/texture/floor1.bmp", "Res/texture/floor2.bmp", "Res/texture/floor3.bmp", "Res/texture/floor4.bmp", "Res/texture/floor5.bmp", "Res/texture/paint1.bmp", "Res/texture/paint2.bmp", "Res/texture/paint3.bmp", "Res/texture/wall1.bmp", "Res/texture/wall2.bmp", "Res/texture/wall3.bmp", "Res/texture/wall4.bmp", "Res/texture/ball.bmp", "Res/texture/carpet.bmp", "Res/texture/wood1.bmp", "Res/texture/wood2.bmp", "Res/texture/quilt.bmp", "Res/texture/door1.bmp", "Res/texture/door2.bmp", "Res/texture/door3.bmp", "Res/texture/doorframe.bmp", "Res/texture/black.bmp", "Res/texture/tree.bmp" }; //23
 string moudelFilepaths[] = { "Res/moudel/moniter.obj", "Res/moudel/computer.obj", "Res/moudel/desk.obj", "Res/moudel/chair.obj", "Res/moudel/curtainoff.obj", "Res/moudel/curtainon.obj", "Res/moudel/floorlamp.obj", "Res/moudel/airconditioner.obj", "Res/moudel/treebottom.obj" }; //9
 
 GLint wallStyle = 9,
